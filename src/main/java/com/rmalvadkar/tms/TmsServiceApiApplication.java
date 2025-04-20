e1adcfdfe702d06d4bdfeadc14f9d5256cba99df
Abhishek Malvadkar <abhi@dev.com>
2025-04-20 17:22:18 +0530

    Initial commit

---
+++ src/main/java/com/rmalvadkar/tms/TmsServiceApiApplication.java
@@ -0,0 +1,13 @@
+package com.rmalvadkar.tms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmsServiceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TmsServiceApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rmalvadkar/tms/TmsServiceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rmalvadkar.tms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmsServiceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
