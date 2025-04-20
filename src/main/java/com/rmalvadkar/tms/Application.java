dcecdf4d36c1f34be1a9710be0572a1cbb5c26d3
Rushieskh <rushi@dev.com>
2025-04-20 17:44:19 +0530

    Just Refactoring

--- src/main/java/com/rmalvadkar/tms/Application.java
+++ src/main/java/com/rmalvadkar/tms/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TmsServiceApiApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TmsServiceApiApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
