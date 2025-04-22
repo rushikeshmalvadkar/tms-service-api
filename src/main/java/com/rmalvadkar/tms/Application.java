cd839b9bb35250deb1050ffba48bab2c0130c02d
Rushikesh <rushi@dev.com>
2025-04-22 06:41:28 +0530

    Create rate limiter using vanilla Java

--- src/main/java/com/rmalvadkar/tms/Application.java
+++ src/main/java/com/rmalvadkar/tms/Application.java
@@ -1,13 +1,91 @@
 package com.rmalvadkar.tms;
 
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 @SpringBootApplication
+@RestController
+@Slf4j
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    private static final Map<String, VisitInfo> IP_ADDRESS_TO_VISIT_INFO_MAP =
+            new ConcurrentHashMap<>();
 
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @GetMapping("/test")
+    public ResponseEntity<String> test(HttpServletRequest request) {
+        /*
+        1 min -> 5
+        6 - Retry after 5 min
+         */
+//        String ipAddress = request.getRemoteAddr();
+//        if (IP_ADDRESS_TO_VISIT_INFO_MAP.containsKey(ipAddress)) {
+//            VisitInfo visitInfo = IP_ADDRESS_TO_VISIT_INFO_MAP.get(ipAddress);
+//            if (requestUnderWindow(visitInfo)) {
+//                int updatedVisitedCount = visitInfo.getVisitCount() + 1;
+//                visitInfo.setVisitCount(updatedVisitedCount);
+//                if (updatedVisitedCount > 5) {
+//                    if (visitInfo.getRateLimitWindowExpiryTime() == null) {
+//                        LocalDateTime rateLimitWindowExpiryTime = LocalDateTime.now().plusMinutes(1);
+//                        visitInfo.setRateLimitWindowExpiryTime(rateLimitWindowExpiryTime);
+//                        visitInfo.setRequestWindowExpiryTime(null);
+//                        return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS)
+//                            .body("Rate Limited : " + rateLimitWindowExpiryTime);
+//                    } else{
+//                        return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS)
+//                                .body("Rate Limited : " + visitInfo.getRateLimitWindowExpiryTime());
+//                    }
+//                }
+//            } else {
+//                if (visitInfo.getRateLimitWindowExpiryTime() != null && visitInfo.getRateLimitWindowExpiryTime()
+//                        .isAfter(LocalDateTime.now())) {
+//                    return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS)
+//                            .body("Rate Limited : " + visitInfo.getRateLimitWindowExpiryTime());
+//                } else {
+//                    visitInfo.setVisitCount(1);
+//                    visitInfo.setRequestWindowExpiryTime(LocalDateTime.now().plusMinutes(1));
+//                    visitInfo.setRateLimitWindowExpiryTime(null);
+//                }
+//            }
+//        } else {
+//            IP_ADDRESS_TO_VISIT_INFO_MAP.put(ipAddress, new VisitInfo(
+//                    LocalDateTime.now(),
+//                    1,
+//                    LocalDateTime.now().plusMinutes(1)
+//            ));
+//        }
+        return ResponseEntity.ok("Hello");
+    }
+
+//    private static boolean requestUnderWindow(VisitInfo visitInfo) {
+//        return visitInfo.getRequestWindowExpiryTime() != null &&
+//                visitInfo.getRequestWindowExpiryTime().isAfter(LocalDateTime.now());
+//    }
+
+}
+
+@Data
+class VisitInfo {
+    private Integer visitCount;
+    private LocalDateTime requestWindowExpiryTime;
+    private LocalDateTime rateLimitWindowExpiryTime;
+
+
+    public VisitInfo(Integer visitCount, LocalDateTime requestWindowExpiryTime) {
+        this.visitCount = visitCount;
+        this.requestWindowExpiryTime = requestWindowExpiryTime;
+    }
 }
---
+++ src/main/java/com/rmalvadkar/tms/RateLimitFilter.java
@@ -0,0 +1,73 @@
+package com.rmalvadkar.tms;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Component
+@Slf4j
+public class RateLimitFilter extends OncePerRequestFilter {
+
+    private static final Map<String, VisitInfo> IP_ADDRESS_TO_VISIT_INFO_MAP =
+            new ConcurrentHashMap<>();
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        log.info("inside rate limit filter");
+        String ipAddress = request.getRemoteAddr();
+        if (IP_ADDRESS_TO_VISIT_INFO_MAP.containsKey(ipAddress)) {
+            VisitInfo visitInfo = IP_ADDRESS_TO_VISIT_INFO_MAP.get(ipAddress);
+            if (requestUnderWindow(visitInfo)) {
+                int updatedVisitedCount = visitInfo.getVisitCount() + 1;
+                visitInfo.setVisitCount(updatedVisitedCount);
+                if (updatedVisitedCount > 5) {
+                    if (visitInfo.getRateLimitWindowExpiryTime() == null) {
+                        LocalDateTime rateLimitWindowExpiryTime = LocalDateTime.now().plusMinutes(1);
+                        visitInfo.setRateLimitWindowExpiryTime(rateLimitWindowExpiryTime);
+                        visitInfo.setRequestWindowExpiryTime(null);
+                        response.setStatus(HttpStatus.TOO_MANY_REQUESTS.value());
+                        response.getWriter().write("Rate Limited : " + rateLimitWindowExpiryTime);
+                        return;
+                    } else{
+                        response.setStatus(HttpStatus.TOO_MANY_REQUESTS.value());
+                        response.getWriter().write("Rate Limited : " + visitInfo.getRateLimitWindowExpiryTime());
+                        return;
+                    }
+                }
+            } else {
+                if (visitInfo.getRateLimitWindowExpiryTime() != null && visitInfo.getRateLimitWindowExpiryTime()
+                        .isAfter(LocalDateTime.now())) {
+                    response.setStatus(HttpStatus.TOO_MANY_REQUESTS.value());
+                    response.getWriter().write("Rate Limited : " + visitInfo.getRateLimitWindowExpiryTime());
+                    return;
+                } else {
+                    visitInfo.setVisitCount(1);
+                    visitInfo.setRequestWindowExpiryTime(LocalDateTime.now().plusMinutes(1));
+                    visitInfo.setRateLimitWindowExpiryTime(null);
+                }
+            }
+        } else {
+            IP_ADDRESS_TO_VISIT_INFO_MAP.put(ipAddress, new VisitInfo(
+                    1,
+                    LocalDateTime.now().plusMinutes(1)
+            ));
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    private static boolean requestUnderWindow(VisitInfo visitInfo) {
+        return visitInfo.getRequestWindowExpiryTime() != null &&
+               visitInfo.getRequestWindowExpiryTime().isAfter(LocalDateTime.now());
+    }
+
+}
